0baa70d9391935bbef3f989ddb353e655c7ff85f
TuoKy <tuomas.kytta@gmail.com>
2015-03-02 13:59:06 +0200

    HTML pohja
    0.0.2

--- adminNakyma.php
+++ adminNakyma.php
@@ -2,24 +2,29 @@
 
     <meta charset="UTF-8"></meta>
     <meta content="user-scalable=no,initial-scale=1.0" name="viewport"></meta>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"></link>
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato"></link>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato"></link>
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster"></link>
     <link rel="stylesheet" href="styles.css"></link>
     <title>Admin paneeli</title>
 
 </head>
 <body>
-
     <div class="application">
-
         <div class="sidebar">
             <div class="logo-container">
                 <div class="logo">
-				Admin
+				Admin panel
 				</div>
             </div>
-			SivuBar
+			<div class="btn-group-vertical">
+				<button type="button" class="btn btn-primary" class="sideButton">Statistics</button>
+				<button type="button" class="btn btn-primary" class="sideButton">Users</button>
+				<button type="button" class="btn btn-primary" class="sideButton">Something</button>
+			</div>
         </div>
         <div class="view view-workspace">
 		<div class ="topbar">
