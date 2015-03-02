d8bc6f2dd57af0c077ad7094658ef893b5e6f601
TuoKy <tuomas.kytta@gmail.com>
2015-03-02 13:18:44 +0200

    HTML pohja
    Versio 0.0.1

---
+++ adminNakyma.php
@@ -0,0 +1,34 @@
+<head>
+
+    <meta charset="UTF-8"></meta>
+    <meta content="user-scalable=no,initial-scale=1.0" name="viewport"></meta>
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"></link>
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato"></link>
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster"></link>
+    <link rel="stylesheet" href="styles.css"></link>
+    <title>Admin paneeli</title>
+
+</head>
+<body>
+
+    <div class="application">
+
+        <div class="sidebar">
+            <div class="logo-container">
+                <div class="logo">
+				Admin
+				</div>
+            </div>
+			SivuBar
+        </div>
+        <div class="view view-workspace">
+		<div class ="topbar">
+		TopBar
+		</div>
+        <div class="board-list">
+				boardList
+        </div>
+        </div>
+    </div>
+</body>
+</html>
