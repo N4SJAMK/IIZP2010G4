fce2ee2871cf382059335ebd27b7f70e59c0a42b
TuoKy <tuomas.kytta@gmail.com>
2015-04-05 00:37:10 +0300

    vagrant upp, vagrant ssh ja se vaan toimii
    Kyllä siinä saatana kesti

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/env bash
 
+PROJECTFOLDER='dev'
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+sudo apt-get install -y apache2
+sudo apt-get install -y php5
 sudo apt-get install -y php5-cli php5-mongo mongodb-server
 
-apt-get update
-apt-get install -y apache2
+# setup hosts file
+VHOST=$(cat <<EOF
+<VirtualHost *:80>
+    DocumentRoot "/var/www/html/${PROJECTFOLDER}"
+    <Directory "/var/www/html/${PROJECTFOLDER}">
+        AllowOverride All
+        Require all granted
+    </Directory>
+</VirtualHost>
+EOF
+)
+echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+
+sudo a2enmod rewrite
+
+service apache2 restart
